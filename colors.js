9b43e3138861d3b6517defe5e1dd78a3c4097859
redlixxt <119645715+redlixxt@users.noreply.github.com>
2024-05-31 17:53:44 -0700

    jQuery by CDN

--- colors.js
+++ colors.js
@@ -1,20 +1,24 @@
 var Links = {
     setColor: function(color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i < alist.length) {
-            console.log(alist[i]);
-            alist[i].style.color = color;
-            i++;
-        }
+        // Command + / : 주석 처리
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i < alist.length) {
+        //     console.log(alist[i]);
+        //     alist[i].style.color = color;
+        //     i++;
+        // }
+        $('a').css('color', color);
     }
 }
 var Body = {
     setColor: function(color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 function nightDayHandler(self) {
@@ -24,13 +28,13 @@ function nightDayHandler(self) {
         Body.setColor('white');
         self.value = 'Day';
 
-        Links.setColor('powderblue');
+        Links.setColor('white');
     }
     else {
         Body.setBackgroundColor('white');
         Body.setColor('black');
         self.value = 'Night';
 
-        Links.setColor('blue');
+        Links.setColor('black');
     }
 }
